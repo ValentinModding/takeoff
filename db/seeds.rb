cc0c1aa357cfe88ce2f9b9b9824ecfefa8d37b3f
mathilde perriol <mathilde.perriol@kedgebs.com>
2022-06-16 16:53:31 +0200

    Seeds Sessions fait

--- db/seeds.rb
+++ db/seeds.rb
@@ -109,3 +109,101 @@ contact1 = Contact.create!(
   user: user1
 )
 puts "contacts crées avec succes"
+
+session1 = Activity.create!(
+  id: 3,
+  wave_height: 2,
+  wind: 6,
+  flag: "Yellow",
+  period: 12,
+  wind_direction: "SE",
+  wave_direction: "E",
+  min_score: 250,
+  date_time_start: "Sat, 18 Jun 2022 08:28:00.000000000 UTC +00:00",
+  date_time_end: "Sat, 18 Jun 2022 14:28:00.000000000 UTC +00:00",
+  tips: "L'idéal pour ce spot est de surfer à marrée haute en raison de ses rochers et de la puissance des vagues.",
+  photo: nil,
+  spot_id: 6,
+  name: "17 Juin - La Grande Plage",
+  temp: 35,
+  water_temp: 22
+)
+
+
+session2 = Activity.create!(
+  id: 4,
+  wave_height: 1,
+  wind: 18,
+  flag: "Green",
+  period: 6,
+  wind_direction: "E",
+  wave_direction: "O",
+  min_score: 250,
+  date_time_start: "Sun, 19 Jun 2022 09:27:00.000000000 UTC +00:00",
+  date_time_end: "Sun, 19 Jun 2022 15:22:00.000000000 UTC +00:00",
+  tips: "Attention aux forts courants (bahines) qui peuvent vous entraîner au large ou vous décaler du spot très rapidement.",
+  photo: nil,
+  spot_id: 2,
+  name: "19 Juin - Les Culs Nuls",
+  temp: 24,
+  water_temp: 19
+)
+
+session3 = Activity.create!(
+  id: 5,
+  wave_height: 1,
+  wind: 11,
+  flag: "Yellow",
+  period: 4,
+  wind_direction: "SE",
+  wave_direction: "SO",
+  min_score: nil,
+  date_time_start: "Sat, 18 Jun 2022 08:28:00.000000000 UTC +00:00",
+  date_time_end: "Sat, 18 Jun 2022 14:28:00.000000000 UTC +00:00",
+  tips: "Les vagues à Anglet sont plus puissantes qu'à Biarritz, mais ce spot est tout à fait praticable pour des niveaux débutants à intermédiaires.",
+  photo: nil,
+  spot_id: 5,
+  name: "18 Juin - Plage de la Petite Chambre d'Amour",
+  temp: 37,
+  water_temp: 22
+)
+
+session4 = Activity.create!(
+  id: 6,
+  wave_height: 1,
+  wind: 9,
+  flag: "Green",
+  period: 10,
+  wind_direction: "NO",
+  wave_direction: "NO",
+  min_score: 0,
+  date_time_start: "Sun, 19 Jun 2022 09:25:00.000000000 UTC +00:00",
+  date_time_end: "Sun, 19 Jun 2022 15:23:00.000000000 UTC +00:00",
+  tips: "Spot idéal pour les débutants à la frontière de l'Espagne !",
+  photo: nil,
+  spot_id: 10,
+  name: "19 Juin - Plage d'Hendaye",
+  temp: 36,
+  water_temp: 24
+)
+
+session5 = Activity.create!(
+  id: 2,
+  wave_height: 1,
+  wind: 10,
+  flag: "Green",
+  period: 11,
+  wind_direction: "SE",
+  wave_direction: "E",
+  min_score: 50,
+  date_time_start: "Fri, 17 Jun 2022 06:41:00.000000000 UTC +00:00",
+  date_time_end: "Fri, 17 Jun 2022 12:47:00.000000000 UTC +00:00",
+  tips: "L'idéal pour ce spot est de surfer à marrée basse car il n'y a pas de plage à marrée haute ce qui peut rendre la sortie de l'eau très difficile.",
+  photo: nil,
+  spot_id: 7,
+  name: "17 Juin - La Cote des Basques",
+  temp: 28,
+  water_temp: 21
+)
+
+puts "sessions créees avec succes"
