00b83c0b2019b4098e48f905319ebea95d3f8895
Julien Leroux <julien.lerx@gmail.com>
2022-06-22 10:49:31 +0200

    fix message syntax + sens sms confiramtion sms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_084246) do
+ActiveRecord::Schema.define(version: 2022_06_22_084218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 2022_06_17_084246) do
     t.integer "score"
     t.string "address"
     t.integer "age"
+    t.string "tel"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
